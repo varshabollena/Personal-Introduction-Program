c67b0b34f9f28760d0449fe55696a170f5182791
Bollena Varsha <varshabollena@gmail.com>
2026-03-10 20:32:45 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,8 @@
+#Week 1 
+# Personal Introduction Program
+name=input("What is your name?")
+age=input("How old are you?")
+hobby=input("What is your favourite hobby?")
+
+print(f"\n🎉 Welcome {name}! 🎉 ")
+print(f"You are {age} years old and love {hobby}.")
